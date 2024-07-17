dcfea2b98dcded227ceaecbc9ab6af7783901972
MatteoDiMario96 <dm.matteo96@live.it>
2024-07-17 21:13:01 +0200

    Create le due rotte con le classi oggetto controller appartenenti. E dato un nome appropiato.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Guest\HomeController;
+use App\Http\Controllers\Guest\FilmController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('pages.guest.home');
-});
+Route::get('/', [HomeController::class, 'view'])->name('guest-home');
+
+
+Route::get('/movies',[FilmController::class, 'index'])->name('guest-film-list');
