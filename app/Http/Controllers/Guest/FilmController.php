e712cc3753a8b4468b4945c7c02e1896ae037e04
MatteoDiMario96 <dm.matteo96@live.it>
2024-07-17 21:14:50 +0200

    Creato un FilmController. Con una funzione index. Al quale ho assegnato una variabile movies che mi prende grazie alla classe ogetto Movie tutti i dati della tabella movies. E la ritorna in pagina compattati.

---
+++ app/Http/Controllers/Guest/FilmController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Guest ;
+use App\Models\Movie;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class FilmController extends Controller
+{
+    public function index(){
+        $movies = Movie::all();
+
+        return view('pages.guest.film', compact('movies'));
+    }
+}
